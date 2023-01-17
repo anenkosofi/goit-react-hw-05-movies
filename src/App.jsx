88122a12b79aefc376c2c13717cf2eda88e7158f
sofidorosh <108129293+anenkosofi@users.noreply.github.com>
2023-01-17 15:10:45 +0200

    Add Navigate to

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { lazy } from 'react';
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route, Navigate } from 'react-router-dom';
 import { SharedLayout } from 'components/SharedLayout';
 import { GlobalStyle } from 'GlobalStyle';
 
@@ -21,7 +21,7 @@ export const App = () => {
             <Route path="cast" element={<Cast />} />
             <Route path="reviews" element={<Reviews />} />
           </Route>
-          <Route path="*" element={<Home />} />
+          <Route path="*" element={<Navigate to="/" />} />
         </Route>
       </Routes>
     </div>
